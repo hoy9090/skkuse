26ff21092e9904e069933be895c02da92f2099ff
sangkyulee <sklee7753@gmail.com>
2016-05-20 19:36:05 +0900

    main mysql

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+var pool = mysql.createPool({
+	host: 'localhost',
+	user: 'root',
+	password: '1234'
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	pool.getConnection(function(conn, err) {
+		if (err)
+			console.error(err);
+		conn.query('use skkuse');
+		conn.query('select * from community', function(err, result, field) {
+			if (err) {
+				console.error(err);
+			}
+			console.log(err, result, field);
+			conn.release();
+			res.render('index');
+		});
+	});
 });
 
 module.exports = router;
